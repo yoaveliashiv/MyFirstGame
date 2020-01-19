464dd1b968007e307be922ebfbd732cce5c28e41
yoaveliashiv <57868000+yoaveliashiv@users.noreply.github.com>
2020-01-19 22:28:24 +0200

    Add files via upload

---
+++ src/Tester/AlgoGameRooboteStartTestJUnit.java
@@ -0,0 +1,14 @@
+package Tester;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlgoGameRooboteStartTestJUnit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Tester/AlgoGameWhereTheRoobotsGoTestJUint.java
@@ -0,0 +1,14 @@
+package Tester;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlgoGameWhereTheRoobotsGoTestJUint {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
